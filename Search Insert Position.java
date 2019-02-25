53f6a5fc4028e9eaa8998d9c344bcecb041b1f7f
Ayush Patel <ayushpatel95@gmail.com>
2019-02-25 00:07:26 -0500

    Update Optimized solution

--- Search Insert Position.java
+++ Search Insert Position.java
@@ -1,20 +1,12 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int index = 0;
-        if(nums.length == 0) return 0;
-        for( int i =0; i< nums.length; i++){
-            if(nums[i] == target){
-                index = i;
-                break;
-        }
-            else if(nums[i] > target){
-                index = i;
-                break;
-            } 
-            if( i == nums.length -1){
-                index = nums.length;
+        for( int i = 0; i < nums.length; i++ ){
+            if (nums[i] >= target){
+                return i;
+            } else if (i == nums.length - 1){
+                return nums.length;
             }
         }
-        return index;
+        return 0;
     }
 }
